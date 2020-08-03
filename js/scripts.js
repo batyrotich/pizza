2d8d34f49577d3bf20cf3e9657990018cb590552
eddah <eddahbaty98@gmail.com>
2020-08-03 09:42:55 +0300

    completed calling functions in the scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,39 @@
 //declaring variables
 var pizzaFlavour = ["Hawaiian", "Peri-peri Chicken", "Chicken Tikka", 
 "Vegetable Tikka", "Butternut Squash"];
+
+var pizzaSize = ["Small", "Medium", "Large"];
+var pizzaCrust = ["Thin crust", "Thick crust", "Gluten-free crust", "Stuffed crust"];
+var pizzaTopping = ["Mushroom", "Onions", "Olives", "Bacon", "Periperi Chicken"];
+
+//calling functions
+function getFlavour() {
+    var flavour = $("#pizzaflavour : selected").val();
+    return parseInt(flavour);
+}
+
+function getSize() {
+    var size = $("#pizzaSize : selected").val();
+    return parseInt(size);
+}
+
+function getCrust() {
+    var crust = $("#pizzacrust : selected").val();
+    return parseInt(crust);
+}
+
+function getTopping() {
+    var topping = $("#pizzatopping : selected").val();
+    return parseInt(topping);
+}
+
+function getNumber(){
+    var number = $("#number").val();
+    return parseInt(number);
+}
+
+
+
+
+
+
